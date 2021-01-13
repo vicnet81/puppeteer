b7f22d1e2028a6120a39390f9d40b4c2bea7e833
Vicente Pires Lustosa Neto <vicente.neto@academico.ifrn.edu.br>
2021-01-13 11:01:36 -0300

    up

--- puppeteer.js
+++ puppeteer.js
@@ -6,8 +6,7 @@ let main = async () => {
     
     const browser = await puppeteer.launch({headless: true,defaultViewport:{width:1024,height:768}})
     const page = (await browser.pages())[0];
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
-    
+ 
     await page.goto("https://suap.ifrn.edu.br");
     
     await page.waitForSelector("#id_username");
